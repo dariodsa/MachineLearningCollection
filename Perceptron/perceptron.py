4f3d3d90b335105545c9a238873caf3435f3285f
Dario Sindičić <dario.sindicic@gmail.com>
2018-11-17 13:21:38 +0100

    printing updates

--- Perceptron/perceptron.py
+++ Perceptron/perceptron.py
@@ -16,23 +16,45 @@ def perceptron_two_classes( samples, C, W):
                 sample.X[i] *= -1
     num = 0
     it = 0
+    id = 0
     while True:
-        print("{0}th iteration\n\n".format(it))
+        print("\n{0}th iteration\n".format(it))
         for idx, sample in enumerate(samples):
             if np.dot(W, sample.X) <= 0:
                 print("Weight correction for the {0} th sample.\n".format(idx+1))
                 W = np.add(W, C*sample.X)
-                print("New weights are: ", W)
+                print("W{0} = W{1} + {2}*X{3} = {4}".format(id+1,id,C,idx+1,W))
                 num = 0
             else:
-                print("No correction for sample {0}".format(idx+1))
+                print("W{0} = W{1}".format(id+1, id))
                 num += 1
+            id += 1
+
         if num >= len(samples):
             break
         it+=1
 
     print("Done")
     print("Final weights are: ", W)
+
+def perceptron_N_classes( samples, C, W):
+    for sample in samples:
+        sample.X.append(1)
+
+    it = 0
+    num = 0
+    num_of_classes = len(W)
+
+    while True:
+        print("{0}th iteration\n\n".format(it))
+        
+        for idx, sample in enumerate(samples):
+            cls = -1
+            max_val = float("inf")
+            for i in range(len(num_of_classes)):
+                val = np.dot(W[i], sample.X)
+
+        it += 1
 
 def main():
 
